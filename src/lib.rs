0e06e9f8cc217f16bc6ae084043a4229b3780a9a
Parsa Ghadimi <i@parsa.ooo>
2023-04-18 11:24:03 -0400

    ep2: Add snapshot functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, RwLock,
+    },
+};
 
 type Key = Box<[u8]>;
 type Value = Box<[u8]>;
@@ -8,7 +14,20 @@ pub struct Env {
 }
 
 struct EnvInner {
-    entires: dashmap::DashMap<Key, Value, fxhash::FxBuildHasher>,
+    version: AtomicUsize,
+    entries: dashmap::DashMap<Key, Value, fxhash::FxBuildHasher>,
+    snapshot_head: std::sync::RwLock<Option<Snapshot>>,
+}
+
+#[derive(Clone)]
+struct Snapshot {
+    inner: Arc<SnapshotInner>,
+}
+
+struct SnapshotInner {
+    version: usize,
+    entries: dashmap::DashMap<Key, Operation, fxhash::FxBuildHasher>,
+    next: std::sync::RwLock<Option<Snapshot>>,
 }
 
 #[derive(Clone)]
@@ -21,10 +40,15 @@ pub struct UpdateRunner {
 }
 
 pub struct Ctx {
+    /// The host environment which we're running the job from.
     env: Arc<EnvInner>,
+    /// If `None` it means the context is running an update and not a query.
+    snapshot: Option<Snapshot>,
+    /// The current batch that is constructed during this job.
     batch: HashMap<Key, Operation, fxhash::FxBuildHasher>,
 }
 
+#[derive(Clone, Debug)]
 enum Operation {
     Remove,
     Insert(Value),
@@ -35,7 +59,9 @@ impl Env {
     pub fn new() -> Self {
         Env {
             inner: Arc::new(EnvInner {
-                entires: Default::default(),
+                version: AtomicUsize::new(0),
+                entries: Default::default(),
+                snapshot_head: RwLock::new(None),
             }),
         }
     }
@@ -62,6 +88,7 @@ impl QueryRunner {
         F: Fn(&mut Ctx) -> T,
     {
         let mut ctx = Ctx::new(self.env.clone());
+        ctx.snapshot = Some(self.env.create_new_snapshot());
         job(&mut ctx)
     }
 }
@@ -81,19 +108,68 @@ impl UpdateRunner {
 
         // commit the batch.
         // TOOD: Skip the commit if the execution failed.
-        self.commit(ctx.batch);
+
+        let env2 = self.env.clone();
+        let lock = env2.snapshot_head.write().unwrap();
+        let maybe_snapshot = &*lock;
+
+        if let Some(snapshot) = maybe_snapshot {
+            self.commit_with_snapshot(ctx.batch, snapshot);
+        } else {
+            self.commit_without_snapshot(ctx.batch);
+        }
+
+        self.env.version.fetch_add(1, Ordering::Relaxed);
 
         result
     }
 
-    fn commit(&mut self, batch: HashMap<Key, Operation, fxhash::FxBuildHasher>) {
+    fn commit_with_snapshot(
+        &mut self,
+        batch: HashMap<Key, Operation, fxhash::FxBuildHasher>,
+        snapshot: &Snapshot,
+    ) {
+        for (key, operation) in batch {
+            let previous_value = self.env.entries.get(&key).map(|c| c.clone());
+
+            match (operation, previous_value) {
+                (Operation::Remove, None) => {}
+                (Operation::Remove, Some(val)) => {
+                    snapshot
+                        .inner
+                        .entries
+                        .insert(key.clone(), Operation::Insert(val));
+
+                    self.env.entries.remove(&key);
+                }
+                (Operation::Insert(value), None) => {
+                    snapshot
+                        .inner
+                        .entries
+                        .insert(key.clone(), Operation::Remove);
+
+                    self.env.entries.insert(key, value);
+                }
+                (Operation::Insert(new_value), Some(prev_value)) => {
+                    snapshot
+                        .inner
+                        .entries
+                        .insert(key.clone(), Operation::Insert(prev_value));
+
+                    self.env.entries.insert(key, new_value);
+                }
+            }
+        }
+    }
+
+    fn commit_without_snapshot(&mut self, batch: HashMap<Key, Operation, fxhash::FxBuildHasher>) {
         for (key, operation) in batch {
             match operation {
                 Operation::Remove => {
-                    self.env.entires.remove(&key);
+                    self.env.entries.remove(&key);
                 }
                 Operation::Insert(value) => {
-                    self.env.entires.insert(key, value);
+                    self.env.entries.insert(key, value);
                 }
             }
         }
@@ -104,20 +180,86 @@ impl Ctx {
     pub(crate) fn new(inner: Arc<EnvInner>) -> Self {
         Self {
             env: inner,
+            snapshot: None,
             batch: Default::default(),
         }
     }
 
     pub fn get(&self, key: &[u8]) -> Option<Value> {
-        match self.batch.get(key) {
-            Some(Operation::Remove) => None,
-            Some(Operation::Insert(value)) => Some(value.clone()),
-            None => self.env.entires.get(key).map(|c| c.clone()),
+        let mut operation = self.batch.get(key).cloned();
+
+        if operation.is_none() && self.snapshot.is_some() {
+            operation = self.snapshot.as_ref().unwrap().get(key);
+        }
+
+        match operation {
+            Some(Operation::Remove) => return None,
+            Some(Operation::Insert(value)) => return Some(value),
+            None => self.env.entries.get(key).map(|c| c.clone()),
         }
     }
 
     pub fn set(&mut self, key: Key, value: Value) {
         self.batch.insert(key, Operation::Insert(value));
+    }
+}
+
+impl Snapshot {
+    pub fn get(&self, key: &[u8]) -> Option<Operation> {
+        let result = self.inner.entries.get(key).map(|c| c.clone());
+
+        if result.is_some() {
+            result
+        } else {
+            let maybe_next = { self.inner.next.read().unwrap().clone() };
+            if let Some(next) = maybe_next {
+                next.get(key)
+            } else {
+                None
+            }
+        }
+    }
+}
+
+impl EnvInner {
+    /// Returns the current snapshot if still the same version in the state or create a
+    /// new empty one.
+    pub fn create_new_snapshot(&self) -> Snapshot {
+        {
+            let read_lock = self.snapshot_head.read().unwrap();
+            let maybe_head = &*read_lock;
+            let version = self.version.load(Ordering::Relaxed);
+
+            if let Some(head) = maybe_head {
+                if head.inner.version == version {
+                    return head.clone();
+                }
+            }
+        }
+
+        let mut lock = self.snapshot_head.write().unwrap();
+        let maybe_head = &*lock;
+        let version = self.version.load(Ordering::Relaxed);
+        let new_head = Snapshot {
+            inner: Arc::new(SnapshotInner {
+                version,
+                entries: Default::default(),
+                next: RwLock::new(None),
+            }),
+        };
+
+        if let Some(head) = maybe_head {
+            if head.inner.version == version {
+                return head.clone();
+            } else {
+                let mut head_next_lock = head.inner.next.write().unwrap();
+                let prev_next = head_next_lock.replace(new_head.clone());
+                debug_assert!(prev_next.is_none());
+            }
+        }
+
+        lock.replace(new_head.clone());
+        new_head
     }
 }
 
@@ -170,7 +312,7 @@ fn demo() {
             std::thread::sleep(std::time::Duration::new(2, 0));
             println!("woke up!");
             let b = balance(ctx, [0; 32]);
-            println!("balance = {b}");
+            println!("balance after wake up = {b}");
         });
     });
 
